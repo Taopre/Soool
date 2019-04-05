2988c019faa3f1a417491869176a7495cd87e941
bung428 <goo428@gmail.com>
2019-04-05 19:03:28 +0900

    QnaBoard로 모델 뷰 프레젠터 수정함.

--- app/src/main/java/com/example/taopr/soool/Presenter/QnaBoardInter.java
+++ app/src/main/java/com/example/taopr/soool/Presenter/QnaBoardInter.java
@@ -2,10 +2,10 @@ package com.example.taopr.soool.Presenter;
 
 import com.example.taopr.soool.Object.QnaBoardItem;
 
-public interface QnaDrawUpInter {
+public interface QnaBoardInter {
     // Presenter 부분쪽에 있는 함수.
 
-    void setView(QnaDrawUpPresenter.View view);
+    void setView(QnaBoardPresenter.View view);
     // QnaDrawUpActivity로 부터 객체를 QnaDrawUpModel로 보내주는 중간다리 함수.
     void enrollmentReq(QnaBoardItem item);
 
--- app/src/main/java/com/example/taopr/soool/Presenter/QnaBoardPresenter.java
+++ app/src/main/java/com/example/taopr/soool/Presenter/QnaBoardPresenter.java
@@ -3,27 +3,27 @@ package com.example.taopr.soool.Presenter;
 import android.app.Activity;
 import android.content.Context;
 
-import com.example.taopr.soool.Model.QnaDrawUpModel;
+import com.example.taopr.soool.Model.QnaBoardModel;
 import com.example.taopr.soool.Object.QnaBoardItem;
 
-public class QnaDrawUpPresenter implements QnaDrawUpInter {
+public class QnaBoardPresenter implements QnaBoardInter {
 
     private Activity activity;
-    private QnaDrawUpPresenter.View view;
-    private QnaDrawUpModel qnaDrawUpModel;
+    private QnaBoardPresenter.View view;
+    private QnaBoardModel qnaBoardModel;
 
-    public QnaDrawUpPresenter(Activity activity, Context context) {
+    public QnaBoardPresenter(Activity activity, Context context) {
         this.activity = activity;
-        this.qnaDrawUpModel = new QnaDrawUpModel(this, context);
+        this.qnaBoardModel = new QnaBoardModel(this, context);
     }
 
     @Override
-    public void setView(QnaDrawUpPresenter.View view) {
+    public void setView(QnaBoardPresenter.View view) {
         this.view = view;
     }
     // QnaActivity로부터 게시물 관련 데이터들을 객체로 받아서 서버로 저장하는 함수.
     @Override
     public void enrollmentReq(QnaBoardItem item) {
-        qnaDrawUpModel.enrollmentReqFromView(item);
+        qnaBoardModel.enrollmentReqFromView(item);
     }
 }
