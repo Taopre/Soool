2d0928fdf70d0e95fa44517482bb7f559e55eab4
bung428 <goo428@gmail.com>
2019-06-26 16:22:04 +0900

    Qna 화면 두 곳 에러수정.

--- app/src/main/java/com/example/taopr/soool/Util/Keyboard.java
+++ app/src/main/java/com/example/taopr/soool/Util/Keyboard.java
@@ -4,16 +4,22 @@ import android.content.Context;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
-public class HideKeyboard {
+public class Keyboard {
     private Context context;
     private InputMethodManager inputMethodManager;
 
-    public HideKeyboard(Context context) {
+    public Keyboard(Context context) {
         this.context = context;
         inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
     }
 
     public void hideKeyboard(View view) {
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public void showKeyboard(View view) {
+        if (view.requestFocus()) {
+            inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+        }
     }
 }
