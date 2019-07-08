4bdb387d77a16ba1ec7a2a11d46b39dcb737032a
Taopre <jtp802@gmail.com>
2019-07-08 16:20:52 +0900

    notice gitIgnore 수정

--- app/src/main/java/com/example/taopr/soool/View/NoticeActivity.java
+++ app/src/main/java/com/example/taopr/soool/View/NoticeActivity.java
@@ -57,7 +57,7 @@ public class NoticeActivity extends AppCompatActivity implements NoticeInter.Vie
         actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
         actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
         actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
-        //actionBar.setElevation(0);
+        actionBar.setElevation(0);
 
         //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
         LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
